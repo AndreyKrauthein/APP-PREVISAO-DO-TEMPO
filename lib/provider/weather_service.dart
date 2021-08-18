6f041e37e8f953aa8a4178339c74a60961b075fe
AndreyKrauthein <54869693+AndreyKrauthein@users.noreply.github.com>
2021-08-18 19:33:33 -0300

    [WIP][FEAT][TESTE] - test with Pierry

--- lib/provider/weather_service.dart
+++ lib/provider/weather_service.dart
@@ -4,5 +4,6 @@ Future<Map> getData() async {
   var response =
       await Dio().get("https://api.hgbrasil.com/weather?woeid=456653");
 
-  return response.data;
+  response.data;
+  return "Ola";
 }
